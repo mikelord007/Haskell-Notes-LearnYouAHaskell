5514687eb55f16ff1517832240083b9eb119cf6d
agentmike007 <manujasan23@gmail.com>
2021-07-28 08:09:52 +0530

    custom deriving data types from typeclasses

---
+++ 3typesnTypeclasses.hs
@@ -0,0 +1,37 @@
+--  TYPECLASS DEFINITIONS AND CUSTOM DERIVING FROM THEM:
+
+-- The Eq typeclass is defined as:
+{-
+    class Eq a where  
+    (==) :: a -> a -> Bool  
+    (/=) :: a -> a -> Bool  
+    x == y = not (x /= y)  
+    x /= y = not (x == y)  
+-}
+-- do :info Eq in GHCI to see more details 
+
+
+-- To make a custom data type an instance of a Typeclass:
+data TrafficLight  = Red | Yellow | Green
+
+instance Eq TrafficLight where
+    Red == Red = True
+    Green == Green = True
+    Yellow == Yellow  = True
+    _ == _ = False
+-- While making a type instance of a typeclass, it should fulfil the minimal complete definition
+instance Show TrafficLight where
+    show Red = "Red Light"
+    show Green = "Green Light"
+    show Yellow = "Yellow Light"
+
+
+-- To make typeclasses subclass of another class:
+-- class (Eq a) => Num a where      //here the Type a should be a part of Eq a to be a part of Num
+
+
+-- only concrete types can be made instances of classes:
+instance (Eq m) => Eq (Maybe m) where  
+    Just x == Just y = x == y  
+    Nothing == Nothing = True  
+    _ == _ = False
